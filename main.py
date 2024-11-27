29a7ad7f4ec13e5d57ca1c61a8a354133e7c61f7
Lucian Patrasc <lpatrasc@gmail.com>
2024-11-27 08:42:45 +0200

    6th commit

--- main.py
+++ main.py
@@ -30,12 +30,21 @@ while game_is_on :
 #detect colision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         score.increase_score()
 
 #detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280 :
         game_is_on =False
         score.game_over()
+
+#detect colision with tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            score.game_over()
 
 
 screen.exitonclick()
